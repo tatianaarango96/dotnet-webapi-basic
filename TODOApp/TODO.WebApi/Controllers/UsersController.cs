2495e70bb9ed771af3ced860d4fe0c71631a4c61
Tatiana <tarango.miro@serviciosnutresa.com>
2023-10-04 11:29:56 -0500

    first commit

---
+++ TODOApp/TODO.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TODO.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+     public UsersController() { 
+        }
+        [HttpGet]
+        public IActionResult GetHelloWorld()
+        {
+            return Ok("Hello World");
+
+        }
+
+    }
+}
+
